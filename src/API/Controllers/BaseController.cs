7e44dd11b2e44ed39fbbeff19991a4dc713e1218
Cristian Rodriguez <crodriguez@evopointsolution.com>
2024-09-09 23:19:42 -0400

    feat(presentation): add api project and some refactor

---
+++ src/API/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastCleanArchitecture.API.Controllers;
+
+[ApiController]
+public abstract class BaseController : ControllerBase
+{
+    protected readonly ISender Sender;
+
+    public BaseController(ISender Sender) => this.Sender = Sender;
+}
---
+++ src/Infrastructure/Data/Configurations/TodoItemConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using FastCleanArchitecture.Domain.TodoItems;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FastCleanArchitecture.Infrastructure.Data.Configurations;
+
+internal sealed class TodoItemConfiguration : IEntityTypeConfiguration<TodoItem>
+{
+    public void Configure(EntityTypeBuilder<TodoItem> builder)
+    {
+        builder.ToTable($"{nameof(TodoItem)}s")
+            .HasKey(x => x.Id);
+
+        builder.Property(x => x.Title)
+            .HasMaxLength(200)
+            .IsRequired();
+    }
+}
