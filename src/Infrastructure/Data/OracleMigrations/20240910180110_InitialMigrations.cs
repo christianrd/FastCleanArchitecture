4603150743936600c7b294e9f1c91bb5d3d47843
Cristian Rodriguez <crodriguez@evopointsolution.com>
2024-09-10 14:09:07 -0400

    fix: add oracle migrations and change template value

---
+++ src/Infrastructure/Data/OracleMigrations/20240910180110_InitialMigrations.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FastCleanArchitecture.Infrastructure.Data.OracleMigrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TodoLists",
+                type: "NVARCHAR2(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ModifiedBy",
+                table: "TodoLists",
+                type: "NVARCHAR2(2000)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ModifiedAtUtc",
+                table: "TodoLists",
+                type: "TIMESTAMP(7) WITH TIME ZONE",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "TodoLists",
+                type: "NVARCHAR2(2000)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedAtUtc",
+                table: "TodoLists",
+                type: "TIMESTAMP(7) WITH TIME ZONE",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Colour_Code",
+                table: "TodoLists",
+                type: "NVARCHAR2(2000)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "TodoLists",
+                type: "RAW(16)",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TodoListId",
+                table: "TodoItems",
+                type: "RAW(16)",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TodoItems",
+                type: "NVARCHAR2(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Reminder",
+                table: "TodoItems",
+                type: "TIMESTAMP(7)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Priority",
+                table: "TodoItems",
+                type: "NUMBER(10)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "TodoItems",
+                type: "NVARCHAR2(2000)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ModifiedBy",
+                table: "TodoItems",
+                type: "NVARCHAR2(2000)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ModifiedAtUtc",
+                table: "TodoItems",
+                type: "TIMESTAMP(7) WITH TIME ZONE",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ListId",
+                table: "TodoItems",
+                type: "RAW(16)",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "TodoItems",
+                type: "NVARCHAR2(2000)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedAtUtc",
+                table: "TodoItems",
+                type: "TIMESTAMP(7) WITH TIME ZONE",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "TodoItems",
+                type: "RAW(16)",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TodoLists",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ModifiedBy",
+                table: "TodoLists",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ModifiedAtUtc",
+                table: "TodoLists",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TIMESTAMP(7) WITH TIME ZONE");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "TodoLists",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedAtUtc",
+                table: "TodoLists",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TIMESTAMP(7) WITH TIME ZONE");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Colour_Code",
+                table: "TodoLists",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "TodoLists",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "RAW(16)");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "TodoListId",
+                table: "TodoItems",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "RAW(16)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TodoItems",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Reminder",
+                table: "TodoItems",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TIMESTAMP(7)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Priority",
+                table: "TodoItems",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "NUMBER(10)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Note",
+                table: "TodoItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ModifiedBy",
+                table: "TodoItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ModifiedAtUtc",
+                table: "TodoItems",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TIMESTAMP(7) WITH TIME ZONE");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ListId",
+                table: "TodoItems",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "RAW(16)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "TodoItems",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(2000)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "CreatedAtUtc",
+                table: "TodoItems",
+                type: "datetimeoffset",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "TIMESTAMP(7) WITH TIME ZONE");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "Id",
+                table: "TodoItems",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "RAW(16)");
+        }
+    }
+}
